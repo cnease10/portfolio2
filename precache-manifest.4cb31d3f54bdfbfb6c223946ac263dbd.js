30a935e5662294dd3b2f615c71cb370aa98ca61e
cnease10 <cierra.nease10@gmail.com>
2020-09-03 23:02:12 -0700

    Updates

---
+++ precache-manifest.4cb31d3f54bdfbfb6c223946ac263dbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14c560ddc46ee1540282d6219c9bc8d",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "f65cbf8e572c15cf374e",
+    "url": "/portfolio2/static/css/main.f47629ba.chunk.css"
+  },
+  {
+    "revision": "2c3e898abdc5989d61d5",
+    "url": "/portfolio2/static/js/2.c17b8875.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio2/static/js/2.c17b8875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f65cbf8e572c15cf374e",
+    "url": "/portfolio2/static/js/main.77d27968.chunk.js"
+  },
+  {
+    "revision": "4b059261683b5be75625",
+    "url": "/portfolio2/static/js/runtime-main.1ee5a9c2.js"
+  }
+]);
